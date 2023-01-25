64396cad30cf3c29c8c9cef8f59b3fa29ff50d09
Compte Etude <etude@dinfo68.dinfo.iut-velizy.uvsq.fr>
2023-01-25 16:57:34 +0100

    Ajout d'options pour la date et de champs la classe Todo

--- toudou_starter/toudou/src/toudou/__init__.py
+++ toudou_starter/toudou/src/toudou/__init__.py
@@ -1,5 +1,7 @@
 import click
 import uuid
+import pickle
+import datetime
 
 from dataclasses import dataclass
 
@@ -8,7 +10,8 @@ from dataclasses import dataclass
 class Todo:
     id: int
     task: str
-
+    complete : bool
+    due : datetime.date
 
 @click.group()
 def cli():
@@ -18,5 +21,19 @@ def cli():
 @cli.command()
 @click.option('--task', prompt='Task')
 def display(task: str):
-    todo = Todo(id=uuid.uuid4().int, task=task)
+    todo = Todo(id=uuid.uuid4().int, task=task,complete=False, due=datetime.datetime.now())
     click.echo(todo)
+
+@cli.command()
+@click.option('--task', prompt='Task')
+@click.option('--jour', prompt='Jour ')
+@click.option('--mois', prompt='mois ')
+@click.option('--annee', prompt='annee ')
+def addTask(task: str,jour:str,mois:str,annee:str):
+    
+    date = datetime.date(int(jour),int(mois),int(annee))
+
+    todo = Todo(id=uuid.uuid4().int, task=task,complete=False, due=date)
+    click.echo(todo)
+    click.echo("tache %s date %s ajoutée" % (task,date))
+
