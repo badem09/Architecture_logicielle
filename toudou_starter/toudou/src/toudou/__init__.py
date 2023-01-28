dbb6661741eb7ea8ee12dbcc1d9054a450a6a895
Demba BA <badembajob@gmail.com>
2023-01-28 18:17:11 +0000

    Verif des taches avant de add et removeTask()

--- toudou_starter/toudou/src/toudou/__init__.py
+++ toudou_starter/toudou/src/toudou/__init__.py
@@ -7,6 +7,7 @@ import os
 
 from dataclasses import dataclass
 
+#verifier si on est bien dans archilog/toudou_starter/toudou avec try catch
 
 @dataclass
 class Todo:
@@ -33,17 +34,36 @@ def display():
 @click.option('--task', prompt='Task')
 @click.option('--jour', prompt='Jour ')
 @click.option('--mois', prompt='mois ')
-@click.option('--annee', prompt='annee ')
+@click.option('--annee', prompt='annee ') #Option Importance?
 def addTask(task: str,jour:str,mois:str,annee:str):
-    
+
     date = datetime.date(int(jour),int(mois),int(annee))
-    todo = Todo(id=uuid.uuid4().int, task=task,complete=False, due=date)
-    click.echo(todo)
-    filee = open('tasks/toto' + str(todo.id) + '.pkl', 'wb')
-    pickle.dump(todo, filee)
-    filee.close()
-    click.echo("tache %s date %s ajoutée" % (task,date))
+    liste = os.listdir('tasks')
+    for file in liste:
+        todo = pickle.load(open('tasks/'+file,'rb'))
+        if todo.task == task and todo.due == date:
+            click.echo("Une tâche à la même date et au même nom existe déja.")
+            break
+    else:
+        date = datetime.date(int(jour),int(mois),int(annee))
+        todo = Todo(id=uuid.uuid4().int, task=task,complete=False, due=date)
+        click.echo(todo)
+        filee = open('tasks/toto' + str(todo.id) + '.pkl', 'wb')
+        pickle.dump(todo, filee)
+        filee.close()
+        click.echo("tache %s date %s ajoutée" % (task,date))
     
 
-
-
+@cli.command()
+@click.option('--task', prompt='Task')
+def removeTask(task: str):
+    liste = os.listdir('tasks')
+    for file in liste:
+        todo = pickle.load(open('tasks/'+file,'rb'))
+        if todo.task == task:
+            os.remove('tasks/'+file)
+            click.echo('Tâche supprimée.')
+            break
+    else:
+         click.echo("La tâche '" + task + "' n'éxiste pas ! ")
+    
