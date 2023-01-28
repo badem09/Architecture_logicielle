0baeca522a1f8e31fab328ae9017a5e58479a703
Demba BA <badembajob@gmail.com>
2023-01-28 17:20:20 +0000

    Implementation pickle et storage des taches

--- toudou_starter/toudou/src/toudou/__init__.py
+++ toudou_starter/toudou/src/toudou/__init__.py
@@ -2,6 +2,8 @@ import click
 import uuid
 import pickle
 import datetime
+import glob
+import os
 
 from dataclasses import dataclass
 
@@ -13,16 +15,19 @@ class Todo:
     complete : bool
     due : datetime.date
 
+    def __str__(self):
+        return self.task + " (" + str(self.due) + ") " + "Done = " + str(self.complete)
+
 @click.group()
 def cli():
     pass
 
 
 @cli.command()
-@click.option('--task', prompt='Task')
-def display(task: str):
-    todo = Todo(id=uuid.uuid4().int, task=task,complete=False, due=datetime.datetime.now())
-    click.echo(todo)
+def display():
+    liste = os.listdir('tasks')
+    for task in liste:
+        click.echo(pickle.load(open('tasks/'+task,'rb')))
 
 @cli.command()
 @click.option('--task', prompt='Task')
@@ -32,8 +37,13 @@ def display(task: str):
 def addTask(task: str,jour:str,mois:str,annee:str):
     
     date = datetime.date(int(jour),int(mois),int(annee))
-
     todo = Todo(id=uuid.uuid4().int, task=task,complete=False, due=date)
     click.echo(todo)
+    filee = open('tasks/toto' + str(todo.id) + '.pkl', 'wb')
+    pickle.dump(todo, filee)
+    filee.close()
     click.echo("tache %s date %s ajoutée" % (task,date))
+    
+
+
 
