7beb8e54e337c937b5a01f065ed4252b314de493
Demba BA <badembajob@gmail.com>
2023-03-05 07:17:32 +0100

    Suppression des fichiers générés par l'installation

---
+++ toudou/README.md
@@ -0,0 +1,80 @@
+# Module Architechture logicielle 
+# Projet Toudou
+
+BA Demba INFI2B
+
+## Installation
+
+
+Dans le dossier du projet, rendez vous dans le dossier toudou et executez la commande suivante:
+```bash
+$ pdm install 
+```
+
+## Lancement
+
+Pour lancer le projet, vous pouvez exécuter le fichier toudou/src/toudou/views.py
+
+```bash
+$ python src/toudou/views.py
+```
+Vous devrez ensuite vous rendre dans un navigateur et taper l'adresse suivante : http://127.0.0.1:5000 
+
+
+## Structure du code
+
+L'application web est structurée en 3 parties : views.py (la vue / le controlleur), 
+models.py (le modèle) et services.py (les services).
+
+views.py possède des fonctions de redirections (commençant pas 'redirect') et des fonctions de traitements.
+
+models.py renferme les fonctions relatives à la base de données et aux objets Todo
+
+services.py permet d'importer et d'exporter des tâches au format csv.
+
+Les fonctionnalités présentes sont :
+- la création de tâches et leur sauvegarde
+- la modification de tâches
+- la suppression de tâches
+- l'importation de tâches d'un fichier csv (voir format ci-dessous)
+- l'exportation des tâches dans un fichier csv (1 exportation = 1 nouveau fichier)
+
+
+### Importation csv
+
+Pour que l'application web puisse reconnaitre les tâches de votre fichier .csv, 
+il faut qu'il suive le format suivant:
+
+```
+id,task,complete,due
+1(int), Nom de la tâche, True/False, date au format YYYY-MM-DD
+```
+
+Exemple:
+
+```
+id,task,complete,due
+3,truc,True,2022-02-02
+1,SCsfc,False,2023-03-04
+2,taches 1,False,2023-03-04
+4,kkk,False,2023-03-16
+```
+
+## Limites / Pistes d'améiliorations
+
+En faisant mon auto-critique, je remarques que mon projet possèdes certaines limites telles que :
+
+
+- L'application peut être vulnérable aux injections sql (sécurité des formulaires).
+- Dans la fonction import_csv de views.py, l'importation est réalisé 2 fois.
+Cela est du à la preview. Pour ne lire le fichier csv qu'une seule fois, il est
+possible que je crée un converter (type de donnés transmit avec les balises < a >)
+personalisé.
+- Id des tâches: La fonction getnext_id() génere un nouvel id en ajoutant 1
+à l'id maximum dans la base de données. Cela à ses limites dans le cas où
+l'application est utilisée pendant une longue période ou s'il y a beaucoup 
+tâches.
+- La répetition: le template html pour l'ajout de tâches et celui pour
+les modifier est très similaire ce qui entraine de la répétition. Cependant,
+Celles-ci étant 2 fonctionnalités bien différentes et pour respecter le SOC
+je n'ai pas fusionné ces 2 templates (ajouter.html et modifier.html).
---
+++ toudou/src/toudou/models.py
@@ -0,0 +1,168 @@
+from dataclasses import dataclass
+from datetime import datetime
+from typing import Optional
+import sqlalchemy as db
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, Boolean, Date
+
+TODO_FOLDER = "db"
+metadata_obj = MetaData()
+engine = create_engine("sqlite:///todos.db", echo=True)
+todo_table = Table(
+    "todos",
+    metadata_obj,
+    Column("id", Integer, primary_key=True),
+    Column("task", String(1000), nullable=False),
+    Column("complete", Boolean, nullable=False),
+    Column("due", Date, nullable=True)
+)
+
+
+@dataclass
+class Todo:
+    id: int
+    task: str
+    complete: bool
+    due: Optional[datetime]
+
+    def __eq__(self, other):
+        return self.due.strftime("%d-%m-%Y") == other.due.strftime("%d-%m-%Y") and self.task == other.task
+
+
+def init_db() -> None:
+    metadata_obj.create_all(engine)
+
+
+def exist(todo: Todo) -> bool:
+    todos = get_todos()
+    for t in todos:
+        if todo == t:
+            return True
+    return False
+
+
+def write_to_bd(todo: Todo) -> bool:
+    """
+    Enregistre une tâche [todo] à la base de données si son id n'y est pas.
+    """
+    if not exist(todo):
+        todo.id = get_next_id() if get_todo(todo.id) else todo.id
+        requete = db.Insert(todo_table).values(id=todo.id, task=todo.task, complete=todo.complete, due=todo.due)
+        with engine.connect() as conn:
+            conn.execute(requete)
+            conn.commit()
+        return True
+    return False
+
+
+def update_bd(todo: Todo) -> None:
+    """
+    Modifie une tâche dans la base de données avec son id
+    """
+    requete = db.Update(todo_table).values(id=todo.id, task=todo.task, complete=todo.complete, due=todo.due)
+    requete = requete.where(db.sql.column('id') == todo.id)
+    with engine.connect() as conn:
+        conn.execute(requete)
+        conn.commit()
+
+
+def get_next_id() -> int:
+    """
+    Génere un nouvel id selon ceux des tâches deja enregistrés (max des id +1).
+    Limite: Si le nombre de tâches devient trop grand.
+    """
+    with engine.connect() as conn:
+        result = conn.execute(db.text("""SELECT MAX(id) FROM todos""")).fetchone()
+        conn.commit()
+    if result[0]:
+        return int(result[0]) + 1
+    return 1
+
+
+def create_todo(task: str, complete: bool = False, due: Optional[datetime] = None) -> None:
+    """
+    Crée une tâche.
+    """
+    id = get_next_id()
+    todo = Todo(id=id, task=task, complete=complete, due=due)
+    write_to_bd(todo)
+
+
+def get_todo(par_id: int) -> Todo | bool:
+    """
+    Retourne la tâche (objet Todo) correspondant à l'id (par_id).
+    """
+    col_id = db.sql.column('id')
+    requete = todo_table.select()
+    requete = requete.where(col_id == par_id)
+    result = None
+    with engine.connect() as conn:
+        result = conn.execute(requete).fetchall()
+        conn.commit()
+    if result:
+        return Todo(result[0][0], result[0][1], result[0][2], result[0][3])
+    return False
+
+
+def get_todos() -> list:
+    """
+    Retourne une liste de tâches triées par date sous forme de tuple.
+    Ex : [(id,task,complete,due),...]
+    """
+    requete = todo_table.select()
+    with engine.connect() as conn:
+        result = conn.execute(requete).fetchall()
+        conn.commit()
+    if result:
+        return sorted(result, key=lambda x: x[3])
+    return []
+    # Autre methode :
+    # requete = select(todo_table)
+    # result = engine.connect().execute(requete).fetchall()
+
+
+def update_todo(id: int, task: str, complete: bool, due: Optional[datetime]) -> None:
+    """
+    Modifie une tâche (l'objet Todo) en fonction des paramêtres et l'enregistre dans la bd.
+    """
+    if get_todo(id):
+        todo = get_todo(id)
+        todo.task = task
+        todo.complete = complete
+        due = due.split("-")
+        todo.due = datetime(int(due[0]), int(due[1]), int(due[2])) if due else todo.due  # pourrait être None
+        update_bd(todo)
+
+
+def delete_todo(id: int) -> bool:
+    """
+    Supprime une tâche de la bd en fonction de [id]
+    Retourne True en cas de succès, False en cas d'échec
+    """
+    if get_todo(id):
+        col_id = db.sql.column('id')
+        requete = todo_table.delete()
+        requete = requete.where(col_id == id)
+        with engine.connect() as conn:
+            conn.execute(requete)
+            conn.commit()
+        return True
+    return False
+
+
+def complete_todo(par_id: int) -> None:
+    """
+    Change le champs 'complete' d'un objet Todo en True.
+    """
+    tache = get_todo(par_id)
+    tache.complete = True
+    update_bd(tache)
+
+
+def delete_all() -> None:
+    """
+    Supprime toutes les tâches contenues dans la bd.
+    """
+    requete = todo_table.delete()
+    with engine.connect() as conn:
+        conn.execute(requete)
+        conn.commit()
