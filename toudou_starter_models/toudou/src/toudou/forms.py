8f0d924c171c5eacab8860f10bdc95dc5321a26f
Demba BA <badembajob@gmail.com>
2023-03-30 22:30:13 +0200

    Ajout ':'

--- toudou_starter_models/toudou/src/toudou/forms.py
+++ toudou_starter_models/toudou/src/toudou/forms.py
@@ -6,12 +6,12 @@ from wtforms.validators import InputRequired
 
 class FormModifier(FlaskForm):
     id = HiddenField("id", name="id")
-    task = StringField(label='Intitulé', name="task", validators=[InputRequired()])
-    status = SelectField(label='Status', name="status", choices=["Complétée", "Incomplète"])
-    due = DateField(label="Date",name="due")
+    task = StringField(label='Intitulé : ', name="task", validators=[InputRequired()])
+    status = SelectField(label='Status : ', name="status", choices=["Complétée", "Incomplète"])
+    due = DateField(label="Date : ",name="due")
 
 class FormAjouter(FlaskForm):
-    task = StringField(label='Intitulé', name="task", validators=[InputRequired()])
-    status = SelectField(label='Status', name="status", choices=["Complétée", "Incomplète"],
+    task = StringField(label='Intitulé : ', name="task", validators=[InputRequired()])
+    status = SelectField(label='Status : ', name="status", choices=["Complétée", "Incomplète"],
                          validators=[InputRequired()], default="Incomplète")
-    due = DateField(label="Date", name="due", default=datetime.date.today())
+    due = DateField(label="Date : ", name="due", default=datetime.date.today())
